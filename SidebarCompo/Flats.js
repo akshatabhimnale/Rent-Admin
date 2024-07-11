990ab5c2f388a23cde1474c62227e2605e60343d
akshu12345-crypto <akshata.bhimnale@gmail.com>
2024-07-11 16:20:51 +0530

    flat.js updated

--- SidebarCompo/Flats.js
+++ SidebarCompo/Flats.js
@@ -1,60 +1,133 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
   View,
   Image,
-  FlatList,
-  ActivityIndicator,
+  Modal,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
 } from "react-native";
 
-const Flats = () => {
-  const [allottedFlats, setAllottedFlats] = useState([]);
-  const [loading, setLoading] = useState(true);
+const API_URL = "https://stock-management-system-server-6mja.onrender.com";
+
+const Flats = ({ route, navigation }) => {
+  const { societyId } = route.params;
+  const [flats, setFlats] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [flatName, setFlatName] = useState("");
 
   useEffect(() => {
-    const fetchAllottedFlats = async () => {
-      try {
-        const response = await fetch(
-          "https://stock-management-system-server-6mja.onrender.com/api/flats/allotted"
-        );
-        const data = await response.json();
-        console.log(data); // Log the response data
-        setAllottedFlats(data); // Set the array of allotted flats
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching allotted flats:", error);
-        setLoading(false);
+    fetch(`${API_URL}/api/flats/flats-by-wings/${societyId}`)
+      .then((response) => response.json())
+      .then((data) => setFlats(data))
+      .catch((error) => console.error("Error fetching flats:", error));
+  }, [societyId]);
+
+  const toggleModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch(
+        `${API_URL}/api/flats/add-flats-by-wing/${societyId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name: flatName }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to add flat");
       }
-    };
 
-    fetchAllottedFlats();
-  }, []);
+      const newFlat = await response.json();
+      setFlats([...flats, newFlat]);
+      Alert.alert("Flat added successfully");
+      setFlatName("");
+      setIsModalVisible(false);
+    } catch (error) {
+      console.error("Error adding flat:", error);
+      Alert.alert("Failed to add flat");
+    }
+  };
 
-  const renderItem = ({ item }) => (
-    <View style={styles.flatItem}>
-      <Image
-        source={require("../assets/images/flats.jpg")}
-        style={styles.image}
-      />
-      <View style={styles.flatDetails}>
-        <Text style={styles.flatName}>{item.name}</Text>
-        <Text style={styles.flatStatus}>{ item.flat_status}</Text>
+  const handleFlatPress = (flat) => {
+    navigation.navigate("Userdetails", { flat });
+  };
+
+  const renderFlats = () => {
+    return (
+      <View style={styles.buildingsContainer}>
+        {flats.map((flat) => (
+          <TouchableOpacity
+            key={flat._id}
+            style={styles.buildingItem}
+            onPress={() => handleFlatPress(flat)}
+          >
+            <Image
+              source={require("../assets/images/flats.jpg")}
+              style={styles.buildingImage}
+            />
+            <Text style={styles.buildingName}>{flat.name}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
-    </View>
-  );
-
-  if (loading) {
-    return <ActivityIndicator size="large" color="#0000ff" />;
-  }
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={allottedFlats}
-        renderItem={renderItem}
-        keyExtractor={(item) => item._id}
-      />
+      <Text style={styles.headerText}>Total Flats</Text>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        {renderFlats()}
+      </ScrollView>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => {
+          setIsModalVisible(false);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Add Flat</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Name of Flat"
+              value={flatName}
+              onChangeText={(text) => setFlatName(text)}
+              editable={true}
+            />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.submitButton}
+                onPress={handleSubmit}
+              >
+                <Text style={styles.submitButtonText}>ADD</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.closeButton}
+                onPress={() => setIsModalVisible(false)}
+              >
+                <Text style={styles.closeButtonText}>CLOSE</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableOpacity style={styles.addButton} onPress={toggleModal}>
+        <Text style={styles.addButtonText}>Add Flat</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -64,37 +137,118 @@ export default Flats;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-  },
-  flatItem: {
-    flexDirection: "row",
     alignItems: "center",
-    marginBottom: 16,
+    backgroundColor: "#fff",
+    paddingTop: 30,
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  scrollView: {
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  buildingsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  buildingItem: {
+    width: "48%",
+    marginBottom: 20,
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
     padding: 10,
-    backgroundColor: "#f9f9f9",
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 8,
-    elevation: 2,
   },
-  image: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    marginRight: 16,
+  buildingImage: {
+    width: "90%",
+    height: 120,
+    resizeMode: "cover",
+    borderRadius: 10,
+    marginBottom: 10,
   },
-  flatDetails: {
+  buildingName: {
+    fontSize: 13,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  addButton: {
+    backgroundColor: "#FFBF00",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  addButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%",
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    padding: 10,
+    width: "100%",
+    marginBottom: 20,
+    minHeight: 40,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    marginTop: 20,
+  },
+  submitButton: {
+    backgroundColor: "#27ae60",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    flex: 1,
+    marginRight: 10,
+  },
+  submitButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  closeButton: {
+    backgroundColor: "#e74c3c",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
     flex: 1,
   },
-  flatName: {
-    fontSize: 18,
+  closeButtonText: {
+    color: "#fff",
+    fontSize: 16,
     fontWeight: "bold",
-    color: "#333",
-  },
-  flatStatus: {
-    fontSize: 14,
-    color: "green",
+    textAlign: "center",
   },
 });
